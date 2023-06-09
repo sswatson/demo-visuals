15924883283de056defe229f50d61603eed11eb2
Samuel S. Watson <samuel.s.watson@gmail.com>
2023-06-09 12:18:05 -0400

    Add buttons

--- act1/graph-visualization/src/App.js
+++ act1/graph-visualization/src/App.js
@@ -2,17 +2,29 @@ import "./styles.css";
 import React, { useEffect, useMemo, useRef } from "react";
 import { raw_data as _raw_data } from "./telecom";
 import ForceGraph3D from "react-force-graph-3d";
-import SpriteText from "three-spritetext";
 import styled from "styled-components";
-import * as THREE from 'three';
+import * as THREE from "three";
 
-const RELATIONALAI_BLUE = '#090F4A';
-const RELATIONALAI_ORANGE = '#E1856C';
+const RELATIONALAI_BLUE = "#090F4A";
+const RELATIONALAI_ORANGE = "#E1856C";
 
 const raw_data = _raw_data;
 
 export default function App() {
   const fgRef = useRef();
+  const [showFraud, setShowFraud] = React.useState(false);
+  const [showPagerank, setShowPagerank] = React.useState(false);
+
+  const toggleFraud = React.useCallback(() => {
+    setCooldownTicks(0);
+    setShowFraud((val) => !val);
+  }, []);
+
+  const togglePagerank = React.useCallback(() => {
+    setCooldownTicks(0);
+    setShowPagerank((val) => !val);
+  }, []);
+
   const [filteredNodes, filteredEdges] = useMemo(() => {
     const nodesWithAttributes = {};
     const edgesWithAttributes = {};
@@ -52,7 +64,7 @@ export default function App() {
       const key = `${d.source}-${d.target}`;
       // const weight = edgesWithAttributes[key]?.weight;
       return edgesWithAttributes[key] || d;
-    })// .filter(Boolean);
+    }); // .filter(Boolean);
 
     const filteredNodes = Object.values(nodesWithAttributes).filter((n) =>
       nodesWithEdge.has(n.id)
@@ -85,65 +97,34 @@ export default function App() {
     // .strength(link => 30)
   }, []);
 
-  const nodeThreeObject = React.useCallback((node) => {
+  const nodeThreeObject = React.useCallback(
+    (node) => {
       // Create a sphere geometry, where the radius is based on the pagerank_score
-      const geometry = new THREE.SphereGeometry((node.pagerank_score ?? 1) * 10, 32, 32);
+      const geometry = new THREE.SphereGeometry(
+        (node.pagerank_score ?? 1) * 10,
+        32,
+        32
+      );
       const material = new THREE.MeshPhongMaterial({
         color: node.fraud ? RELATIONALAI_ORANGE : "#DDD",
       });
       return new THREE.Mesh(geometry, material);
-  });
-
-  const handleNodeColor = React.useCallback((node) => {
-    const sprite = new SpriteText(node.label);
-    sprite.color = "white";
-    sprite.textHeight = 0.5;
-    sprite.borderColor = "white";
-    sprite.borderRadius = 6;
-    sprite.padding = 3;
-    sprite.borderWidth = 1;
-    sprite.backgroundColor = "white";
-    sprite.receiveShadow;
-    sprite.castShadow;
-
-    if (node.shape === "pentagon") {
-      sprite.borderColor = "#508BE4";
-      sprite.backgroundColor = "#508BE4";
-    } else if (node.shape === "note") {
-      sprite.borderColor = "#7434E7";
-      sprite.backgroundColor = "#7434E7";
-    } else if (node.shape === "oval") {
-      sprite.borderColor = "#0A91AE";
-      sprite.backgroundColor = "#0A91AE";
-    } else if (node.shape === "house") {
-      sprite.borderColor = "#5ABAEB";
-      sprite.backgroundColor = "#5ABAEB";
-    } else if (node.shape === "hexagon") {
-      sprite.borderColor = "#408CFF";
-      sprite.backgroundColor = "#408CFF";
-    } else if (node.shape === "square") {
-      sprite.borderColor = "#9F84BD";
-      sprite.backgroundColor = "#9F84BD";
-    }
-
-    if (node.color && node.color === "red") {
-      sprite.borderColor = "#FF4539";
-      sprite.backgroundColor = "#FF4539";
-      sprite.color = "white";
-      sprite.textHeight = 10;
-    }
-
-    return sprite;
-  }, []);
+    },
+    [showFraud, showPagerank]
+  );
 
   const linkWidth = React.useCallback((link) => {
     if (link.weight) {
-      return Math.round(5 * link.weight);
+      if (link.weight >= 0.5) {
+        return link.weight;
+      } else {
+        return 0.001;
+      }
     } else {
-      return 1;
+      return 0.001;
     }
   }, []);
-  
+
   const [cooldownTicks, setCooldownTicks] = React.useState(undefined);
 
   useEffect(() => {
@@ -153,64 +134,26 @@ export default function App() {
   }, []);
 
   const linkColors = new Float32Array([
-    255, 255, 255, 1, // source
-    255, 255, 255, 0  // target
+    255,
+    255,
+    255,
+    1, // source
+    255,
+    255,
+    255,
+    0, // target
   ]);
 
   const linkThreeObject = React.useCallback((link) => {
-    const material = new THREE.LineBasicMaterial({ vertexColors: true, transparent: true });
+    const material = new THREE.LineBasicMaterial({
+      vertexColors: true,
+      transparent: true,
+    });
     const geometry = new THREE.BufferGeometry();
     // geometry.setAttribute('position', new THREE.BufferAttribute(new Float32Array(2 * 3), 3));
     // geometry.setAttribute('color', new THREE.BufferAttribute(linkColors, 4));
     return new THREE.Line(geometry, material);
   }, []);
-
-  const CameraOrbit = React.memo(() => {
-    return (
-      <ForceGraph3D
-        ref={fgRef}
-        backgroundColor={RELATIONALAI_BLUE}
-        graphData={{
-          nodes: filteredNodes,
-          links: filteredEdges,
-        }}
-        // nodeLabel={() => ""}
-        nodeThreeObjectExtend={false}
-        nodeThreeObject={nodeThreeObject}
-        // link props
-        // linkDirectionalParticleColor={() => "blue"}
-        // linkDirectionalParticleWidth={3}
-        // linkHoverPrecision={10}
-        linkDirectionalParticles={1}
-        linkDirectionalParticleWidth={1}
-        linkOpacity={0.2}
-        linkWidth={linkWidth}
-        linkDirectionalArrowLength={2}
-        linkDirectionalArrowRelPos={1}
-        linkColor={(link) => {
-          if (link.color) {
-            return link.color;
-          } else {
-            return "#9A9A9A";
-          }
-        }}
-        linkThreeObjectExtend={true}
-        linkThreeObject={linkThreeObject}
-        linkPositionUpdate={(sprite, { start, end }) => {
-          const middlePos = Object.assign(
-            ...["x", "y", "z"].map((c) => ({
-              [c]: start[c] + (end[c] - start[c]) / 2, // calc middle point
-            }))
-          );
-          Object.assign(sprite.position, middlePos);
-        }}
-        // loading optimization
-        // warmupTicks={100}
-        cooldownTicks={cooldownTicks}
-        onNodeClick={handleZoom}
-      />
-    );
-  });
 
   const Container = styled.div`
     width: 160px;
@@ -228,18 +171,138 @@ export default function App() {
     z-index: 99;
   `;
 
+  // this is ugly as sin but if you 
+  // do it the right way it forces
+  // a hard re-render of the graph that 
+  // destroys the desired effect
+  const originalNodes = useMemo(() => {
+    return filteredNodes.map((node) => ({
+      ...node
+    }));
+  }, []);
+
+  if (showFraud) {
+    filteredNodes.forEach((node, j) => {
+      node.fraud = originalNodes[j].fraud;
+    });
+  } else {
+    filteredNodes.forEach((node) => {
+      node.fraud = false;
+    });
+  }
+
+  if (showPagerank) {
+    filteredNodes.forEach((node, j) => {
+      node.pagerank_score = originalNodes[j].pagerank_score;
+    });
+  } else {
+    filteredNodes.forEach((node) => {
+      node.pagerank_score = 0.25;
+    });
+  }
+
+  const graphData = useMemo(
+    () => ({
+      nodes: filteredNodes,
+      links: filteredEdges,
+    }),
+    [showFraud, showPagerank, filteredNodes, filteredEdges]
+  );
+
   return (
     <div className="App">
-      <img style={{
-        width: 200,
-        position: "absolute",
-        top: 10,
-        left: 10,
-        zIndex: 100,
-      }} src={"logo.svg"} alt="RelationalAI Logo"/>
-      <Container>
-      </Container>
-      <CameraOrbit />
+      <ControlPanel showFraud={showFraud} toggleFraud={toggleFraud} showPagerank={showPagerank} togglePagerank={togglePagerank}/>
+      <Container></Container>
+      <ForceGraph3D
+        ref={fgRef}
+        backgroundColor={RELATIONALAI_BLUE}
+        graphData={graphData}
+        // nodeLabel={() => ""}
+        nodeThreeObjectExtend={false}
+        nodeThreeObject={nodeThreeObject}
+        // link props
+        // linkDirectionalParticleColor={() => "blue"}
+        // linkDirectionalParticleWidth={3}
+        // linkHoverPrecision={10}
+        // linkDirectionalParticles={1}
+        // linkDirectionalParticleWidth={1}
+        linkOpacity={0.25}
+        linkWidth={linkWidth}
+        // linkDirectionalArrowLength={2}
+        // linkDirectionalArrowRelPos={1}
+        linkColor={() => "#BBB"}
+        // linkColor={(link) => {
+        //   if (link.color) {
+        //     return link.color;
+        //   } else {
+        //     return "#9A9A9A";
+        //   }
+        // }}
+        // linkThreeObjectExtend={true}
+        // linkThreeObject={linkThreeObject}
+        // linkPositionUpdate={(sprite, { start, end }) => {
+        //   const middlePos = Object.assign(
+        //     ...["x", "y", "z"].map((c) => ({
+        //       [c]: start[c] + (end[c] - start[c]) / 2, // calc middle point
+        //     }))
+        //   );
+        //   Object.assign(sprite.position, middlePos);
+        // }}
+        // loading optimization
+        // warmupTicks={100}
+        cooldownTicks={cooldownTicks}
+        onNodeClick={handleZoom}
+      />
     </div>
   );
 }
+
+const ControlPanel = ({ showFraud, toggleFraud, showPagerank, togglePagerank }) => {
+  return (
+    <>
+      <img
+        style={{
+          width: 200,
+          position: "absolute",
+          top: 10,
+          left: 10,
+          zIndex: 100,
+        }}
+        src={"logo.svg"}
+        alt="RelationalAI Logo"
+      />
+      <button
+        style={{
+          position: "absolute",
+          top: 10,
+          right: 10,
+          zIndex: 100,
+          backgroundColor: "white",
+          border: "none",
+          borderRadius: 5,
+          padding: 10,
+          cursor: "pointer",
+        }}
+        onClick={toggleFraud}
+      >
+        {showFraud ? "Hide Fraud" : "Show Fraud"}
+      </button>
+      <button
+        style={{
+          position: "absolute",
+          top: 10,
+          right: 120,
+          zIndex: 100,
+          backgroundColor: "white",
+          border: "none",
+          borderRadius: 5,
+          padding: 10,
+          cursor: "pointer",
+        }}
+        onClick={togglePagerank}
+      >
+        {showPagerank ? "Hide PageRank" : "Show PageRank"}
+      </button>
+    </>
+  );
+};
